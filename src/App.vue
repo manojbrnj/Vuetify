36ac8f46c2054e6e2e39dea0162a2e6c45924a2c
manoj barange <manojbrnj111@gmail.com>
2022-07-24 12:17:20 +0530

    roture added

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,16 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text> Home </v-btn>
-      <v-btn text> Login </v-btn>
+
+      <router-link to="/">
+        <v-btn text> Home </v-btn>
+      </router-link>
+      <router-link to="/login">
+        <v-btn text> Login </v-btn>
+      </router-link>
     </v-app-bar>
     <!-- Login Module -->
-    <v-main>
-      <v-card width="400px" class="mx-auto mt-5">
-        <v-card-title><h1>HelloWorld</h1> </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field
-              label="username"
-              prepend-icon="mdi-account"
-              background-color="white"
-            ></v-text-field>
-            <v-text-field
-              label="password"
-              :type="showPass ? 'text' : 'password'"
-              prepend-icon="mdi-lock"
-              :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
-              background-color="white"
-              @click:append="showPass = !showPass"
-            ></v-text-field>
-            <v-card-actions>
-              <v-btn x-small color="success">Login</v-btn>
-              <v-spacer></v-spacer>
-              <v-btn x-small color="success">Register</v-btn>
-            </v-card-actions>
-          </v-form>
-        </v-card-text>
-      </v-card>
-    </v-main>
+    <v-main> <router-view></router-view> </v-main>
     <v-footer dark padless>
       <v-card class="flex" flat tile>
         <v-card-text class="py-2 white--text text-center">
@@ -48,9 +27,6 @@
 export default {
   name: "App",
   components: {},
-  data: () => ({
-    showPass: false,
-    links: ["Home", "Login"],
-  }),
+  data: () => ({}),
 };
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title><h1>HelloWorld</h1> </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            label="username"
+            prepend-icon="mdi-account"
+            background-color="white"
+          ></v-text-field>
+          <v-text-field
+            label="password"
+            :type="showPass ? 'text' : 'password'"
+            prepend-icon="mdi-lock"
+            :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+            background-color="white"
+            @click:append="showPass = !showPass"
+          ></v-text-field>
+          <v-card-actions>
+            <v-btn x-small color="success">Login</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn x-small color="success">Register</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  name: "Login",
+  components: {},
+  data: () => ({
+    showPass: false,
+    username: "",
+    password: "",
+    links: ["Home", "Login"],
+  }),
+};
+</script>
+<style lang=""></style>
